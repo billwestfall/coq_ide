9fbb06921aa7566084983149c9efd028459a0a02
billwestfall <billwestfall@yahoo.com>
2015-10-31 16:50:29 -0700

    Create page0279b.v
    page 279 4b

---
+++ 007chapt/page0279b.v
@@ -0,0 +1,17 @@
+
+Sx:             sailor
+Tx:             tennis player
+Yx:             yuppie
+Qx:             squash player
+Lxy:            x likes y
+
+c:              Charles
+l:              Linda
+s:              Stan
+
+
+b.              (Sc /\ ~Tc) /\ (Sl /\ Tl)
+                Yl /\ ~Yc
+                Llc /\ Lsc
+                Ys /\ Llc /\ Lls
+                (Ss /\ Ts /\ Qs) /\ Ls
