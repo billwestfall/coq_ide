de27627dc2281bc49c9533ea05c1e8998c7419c8
billwestfall <billwestfall@yahoo.com>
2015-10-30 18:39:33 -0700

    Update proof013.v

--- 001chapt/proof013.v
+++ 001chapt/proof013.v
@@ -1,6 +1,6 @@
 Premise :   Gq /\ Gj is true or false
 if Zxy where x > 1 , then Gx is true
-or if Mxy where Mxy > 2 then Gx is true
+or if Mxy where Mxy >= 2 then Gx is true
 
 Arq /\ Amq /\ Afq
 Arj /\ Amj /\ Afj
