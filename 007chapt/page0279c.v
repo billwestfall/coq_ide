c3988dd92d3a2ff03712f8f422934cc8aee2fe1f
billwestfall <billwestfall@yahoo.com>
2015-10-31 16:59:47 -0700

    Create page0279c.v
    question 4c

---
+++ 007chapt/page0279c.v
@@ -0,0 +1,18 @@
+
+Hx:             hiker
+Mx:             mountain climber
+Kx:             kayaker
+Sx:             swimmer
+Lxy:            x likes y
+Nxy:            x is nuts about y
+
+a:              Andrew
+c:              Christopher
+d:              Amanda
+
+c.              (Ha /\ Hc) /\ (~Ma /\ ~Mc)
+                (Hd /\ Ma /\ Ka)
+                Ka \/ Kc
+                ~Sa /\ ~Sc /\ ~Sd
+                Lac /\ Lad /\ Lca /\ Lad /\ Lda /\ Ldc
+                Nda /\ Nad
