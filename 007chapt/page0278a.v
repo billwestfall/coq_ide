6916c65d798027714ba324046dbb4db821faa6f9
billwestfall <billwestfall@yahoo.com>
2015-10-31 15:45:48 -0700

    Create page0278a.v

---
+++ 007chapt/page0278a.v
@@ -0,0 +1,17 @@
+
+a.          Bai
+b.          Bct
+c.          Bbn
+d.          Bds
+e.          Beh
+f.          ~Bk
+g.          Aph /\ Ahn /\ Ank
+h.          Apt /\ Aht
+i.          Aih <-> Aip
+j.          Ldp -> Lbp
+k.          Lp /\ ~Bp
+l.          Tbc /\ Tca /\ Teb /\ Tec
+m.          Td(a-c,e-t)
+n.          ~Ta(b-t)
+o.          ~Ta(b-t) /\ A(b-t)
+p.          Tcb -> Lca
