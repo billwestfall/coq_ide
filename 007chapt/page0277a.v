e7bee0634f592c1de583c13699e7d8225c3c9fb3
billwestfall <billwestfall@yahoo.com>
2015-10-31 15:04:06 -0700

    Create page0277a.v
    placeholder for question 1

---
+++ 007chapt/page0277a.v
@@ -0,0 +1,7 @@
+UD:           Herman, Juan, Antonio
+Sxyz:         x is larger than y but smaller than z
+Lxy:          x is larger than y
+Rxy:          x is to the right of y
+a:            Antonio
+h:            Herman
+m:            Juan
