c495089bcd927a11cd025aabd3fff34581966a14
billwestfall <billwestfall@yahoo.com>
2015-10-30 18:51:44 -0700

    Update proof013.v

--- 001chapt/proof013.v
+++ 001chapt/proof013.v
@@ -1,7 +1,7 @@
 Premise :   Gq /\ Gj is true or false
 if Zxy where x > 1 , then Gx is true
 or if Mxy where Mxy >= 2 then Gx is true
-forall x y:G, (M x y >= 2) /\ (C x y > 0) -> G y
+forall x y:G, (Z x y > 1) \/ ((M x y >= 2) /\ (C x y > 0)) -> G y
 
 Arq /\ Amq /\ Afq
 Arj /\ Amj /\ Afj
