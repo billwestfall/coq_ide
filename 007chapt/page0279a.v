8eb14fc8793a92a02931cc64c292e6f7560456f3
billwestfall <billwestfall@yahoo.com>
2015-10-31 16:37:43 -0700

    Create page0279a.v
    question 4a

---
+++ 007chapt/page0279a.v
@@ -0,0 +1,19 @@
+
+Lx:           likes skateboarding
+Gx:           good at skateboarding
+Wx:           wear headgear
+Kx:           wear kneepads
+Rx:           most reckless
+Sx:           most skillful
+
+m:            Margaret
+t:            Todd
+c:            Charles
+s:            Sarah
+
+
+a.            (Lm /\ Lt) /\ (~Gm /\ ~Gt)
+              (Gc /\ ~Lc)
+              (Gs /\ Ls)
+              (W(c-t)) /\ (Kc /\ Ks /\ ~Km /\ ~Kt)
+              Rs /\ Sc
