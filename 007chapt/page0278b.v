994af365e16d9fcbce458b79e41cbcb705dea414
billwestfall <billwestfall@yahoo.com>
2015-10-31 16:20:36 -0700

    Create page0278b.v

---
+++ 007chapt/page0278b.v
@@ -0,0 +1,19 @@
+
+a.          Ia /\ Ba /\ ~Ra
+b.          Rc /\ Bc /\ ~Ic
+c.          Bd /\ Rd /\ Id
+d.          ~Rb /\ ~Bb /\ ~Ib
+e.          Ib -> (Id /\ Ia)
+f.          (Ba /\ Ia) /\ (Ib /\ ~Bb) /\ (~Ra /\ ~Rb)
+g.          Lab /\ Dac
+h.          (Laa /\ Lac) /\ (Dab /\ Dad)
+i.          (~Lca /\ ~Dca) /\ (Lcd /\ Dcd)
+j.          (~Adc /\ ~Abc) /\ (Acd /\ Acb)
+k.          Acb <-> (Sbc /\ Rb)
+l.          (Aab /\ Aad) /\ (~Lab /\ ~Lad)
+m.          (Sdc /\ Sca) -> (Sda)
+n.          (Abd /\ Adb) -> (Lbd /\ Ldb)
+o.          (Lcb /\ Lba) -> (Dca /\ Sca)
+p.          (~Rc /\ ~Bc /\ ~Ic) -> (L(a-b,d)c)
+q.          ~R(a-c)
+r.          ~R(a-c) /\ ~I(a-c)  
