ea579c1e37cc193aca9288521b1392b352dc9b38
billwestfall <billwestfall@yahoo.com>
2015-10-30 18:35:47 -0700

    Create proof013.v
    placeholder for an idea

---
+++ 001chapt/proof013.v
@@ -0,0 +1,9 @@
+Premise :   Gq /\ Gj is true or false
+if Zxy where x > 1 , then Gx is true
+or if Mxy where Mxy > 2 then Gx is true
+
+Arq /\ Amq /\ Afq
+Arj /\ Amj /\ Afj
+Zgq /\ Zgj
+Mrq /\ Mgq /\ Mmq
+Mrj /\ Mgj /\ Mmj
