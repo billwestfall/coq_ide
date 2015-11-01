472062e28bd1d27f3135c7f41c4db312210186e4
billwestfall <billwestfall@yahoo.com>
2015-10-31 17:08:42 -0700

    Create page0279d.v

---
+++ 007chapt/page0279d.v
@@ -0,0 +1,18 @@
+
+Tx:                 tax law
+Mx:                 malpractice law
+Gxy:                x gets better grades than y
+Fx:                 finish in 3 years
+Bx:                 pass bar exam
+Jx:                 job as attorney
+
+j:                  Joan
+m:                  Mark
+a:                  Alice
+r:                  Randy
+
+d.                  (Tj /\ Tr) /\ (Mm /\ Ma)
+                    Gar /\ Gmj
+                    F(a-r)
+                    Bj /\ Ba /\ Br
+                    (Jj /\ Ja) \/ (Ja /\ Jr) \/ (Jj /\ Jr)
