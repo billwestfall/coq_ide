94233872976dd7d486ffd5a8321c0ffccd17f343
billwestfall <billwestfall@yahoo.com>
2015-10-31 20:30:32 -0700

    Update page0286a.v
    better approximation

--- 007chapt/page0286a.v
+++ 007chapt/page0286a.v
@@ -3,14 +3,13 @@ UD:       all x
 Bx:       x is black
 Rx:       x is red
 
-          Bx
-          Bx /\ ~Bx
-          ~Bx
-          Bx /\ ~Bx
-          Bx /\ Rx
-          Bx -> ~Rx
-          Rx -> Bx
-          ~Bx -> Rx
-          Bx <-> ~Rx
-          Bx | Rx
-
+          forall x:A, B x
+          exists x:A, B x
+          forall x:A, ~B x
+          exists x:A, ~B x
+          exists x:A, B x /\ R x
+          forall x:A, Bx -> ~Rx
+          exists x:A, Bx /\ Rx
+          forall x:A, ~B x -> R x
+          forall x:A, B x <-> ~R x
+          forall x:A, B x | R x
