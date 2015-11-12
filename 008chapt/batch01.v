f2807dc74d3863c02dc159f19d3ee4a8f993b249
Bill Westfall <billwestfall@yahoo.com>
2015-11-11 19:05:09 -0800

    Create batch01.v
    just an example of running a Coq file in batch mode, in this case from http://www.cis.upenn.edu/~bcpierce/sf/current/index.html , Basics.v

---
+++ 008chapt/batch01.v
@@ -0,0 +1,26 @@
+MacBook-Air:~ billw$ /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/coqc /Users/billw/Downloads/sf/Basics.v
+     = monday
+     : day
+     = tuesday
+     : day
+true
+     : bool
+negb true
+     : bool
+negb
+     : bool -> bool
+4
+     : nat
+     = 2
+     : nat
+S
+     : nat -> nat
+pred
+     : nat -> nat
+minustwo
+     : nat -> nat
+     = 5
+     : nat
+0 + 1 + 1
+     : nat
+MacBook-Air:~ billw$ 
