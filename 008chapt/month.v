54d2e2d531320114981a8223b6c70ea80b0d2b9e
Bill Westfall <billwestfall@yahoo.com>
2015-11-12 20:55:19 -0800

    Update month.v
    fixed!

--- 008chapt/month.v
+++ 008chapt/month.v
@@ -1,3 +1,47 @@
+(** MacBook-Air:08chapt billw$ /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/coqc -verbose months.v
+Definition admit {T: Type} : T.
+Admitted.
+Inductive month : Type :=
+  | january : month
+  | february : month
+  | march : month
+  | april : month
+  | may : month
+  | june : month
+  | july : month
+  | august : month
+  | september : month
+  | october : month
+  | november : month
+  | december : month.
+Definition next_month (m:month) : month :=
+  match m with
+  | january    => february
+  | february   => march
+  | march      => april
+  | april       => may
+  | may         => june
+  | june        => july
+  | july        => august
+  | august      => september
+  | september   => october
+  | october     => november
+  | november    => december
+  | december    => january
+  end.
+Eval compute in (next_month april).
+     = may
+     : month
+Eval compute in (next_month (next_month may)).
+     = july
+     : month
+Example test_next_month:
+  (next_month (next_month may)) = july.
+Proof.
+simpl.
+reflexivity.
+Qed.
+MacBook-Air:08chapt billw$ *)
 
 Definition admit {T: Type} : T.  Admitted.
 
@@ -37,6 +81,6 @@ Definition next_month (m:month) : month :=
      (* ==> june : month *)
 
 Example test_next_month:
-  (next_month (next_month may)) = june.
+  (next_month (next_month may)) = july.
 
 Proof. simpl. reflexivity.  Qed.
