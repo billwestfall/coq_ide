1dbe11a954fab4245398b728d285769d89b01ea7
Bill Westfall <billwestfall@yahoo.com>
2015-11-18 19:34:08 -0800

    Update page056a.v
    info_auto gives much more useful verbose output so changing to that output and command

--- symbolic/page056a.v
+++ symbolic/page056a.v
@@ -1,4 +1,6 @@
-(* MacBook-Air:~ billw$ /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/coqtop
+(*
+
+MacBook-Air:~ billw$ /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/coqtop
 Welcome to Coq 8.4pl5 (October 2014)
 
 Coq < Section Books.
@@ -29,22 +31,39 @@ Unnamed_thm < elim H.
   ============================
    ~ (x -> m) -> ~ (y -> ~ m) -> ~ (x -> y)
 
-Unnamed_thm < auto.
+Unnamed_thm < info_auto.
+(* info auto : *)
+ intro.
+ intro.
+ unfold not.
+  intro.
+  apply H1.
+   intro.
+   unfold not.
+    intro.
+    apply H1.
+     intro.
+     unfold not.
+     unfold not.
+    apply H0.
+     intro.
+     assumption.
 No more subgoals.
 
 Unnamed_thm < Qed.
 intros.
 elim H.
-auto.
+info_auto.
 
 Unnamed_thm is defined
 
 Coq <
+
 *)
 
 Section Books.
 Goal forall x y m:Prop, (~(x -> m) /\ ~(y -> ~m)) -> ~(x -> y).
 intros.
 elim H.
-auto.
+info_auto.
 Qed.
