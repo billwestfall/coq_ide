db671af5d46b74b88c5b4ade7323a106b8910b73
Bill Westfall <billwestfall@yahoo.com>
2015-11-22 18:49:31 -0800

    Create page0376d.v
    first example of addition (one step more than conjunction)

---
+++ concise/06chapt/page0376d.v
@@ -0,0 +1,50 @@
+(*
+MacBook-Air:~ billw$ /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/coqtop
+Welcome to Coq 8.4pl5 (October 2014)
+
+Coq < Section Addition_A.
+
+Coq < Goal forall s t:Prop, s -> (s \/ ~t).
+1 subgoal
+
+  ============================
+   forall s t : Prop, s -> s \/ ~ t
+
+Unnamed_thm < intros.
+1 subgoal
+
+  s : Prop
+  t : Prop
+  H : s
+  ============================
+   s \/ ~ t
+
+Unnamed_thm < left.
+1 subgoal
+
+  s : Prop
+  t : Prop
+  H : s
+  ============================
+   s
+
+Unnamed_thm < exact H.
+No more subgoals.
+
+Unnamed_thm < Qed.
+intros.
+left.
+exact H.
+
+Unnamed_thm is defined
+
+Coq <
+
+*)
+
+Section Conjunction_B.
+Goal forall s t:Prop, s -> (s \/ ~t).
+intros.
+left.
+exact H.
+Qed.
