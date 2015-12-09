f0fedc112c40d35dc8a869f0968ad41cb07c8b7c
Bill Westfall <billwestfall@yahoo.com>
2015-12-08 19:53:25 -0800

    Create page0385a.v
    first write of associativity

---
+++ concise/07chapt/page0385a.v
@@ -0,0 +1,39 @@
+(*
+MacBook-Air:~ billw$ /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/coqtop
+Welcome to Coq 8.4pl5 (October 2014)
+
+Coq < Section Associativity_A.
+
+Coq < Goal forall p q r:Prop, (p \/ (q \/ r)) <-> ((p \/ q) \/ r).
+1 subgoal
+
+  ============================
+   forall p q r : Prop, p \/ q \/ r <-> (p \/ q) \/ r
+
+Unnamed_thm < intros.
+1 subgoal
+
+  p : Prop
+  q : Prop
+  r : Prop
+  ============================
+   p \/ q \/ r <-> (p \/ q) \/ r
+
+Unnamed_thm < tauto.
+No more subgoals.
+
+Unnamed_thm < Qed.
+intros.
+tauto.
+
+Unnamed_thm is defined
+
+Coq <
+
+*)
+
+Section Associativity_A.
+Goal forall p q r:Prop, (p \/ (q \/ r)) <-> ((p \/ q) \/ r).
+intros.
+tauto.
+Qed.
