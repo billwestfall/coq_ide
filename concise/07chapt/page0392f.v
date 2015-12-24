5b3fc2de7bc10fe4bf1c5f217de4c9852746a19e
Bill Westfall <billwestfall@yahoo.com>
2015-12-24 10:44:50 -0800

    Update page0392f.v
    derp just had to import classical (which i will do for each going forward)

--- concise/07chapt/page0392f.v
+++ concise/07chapt/page0392f.v
@@ -2,6 +2,8 @@
 MacBook-Air:~ billw$ /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/coqtop
 Welcome to Coq 8.4pl5 (October 2014)
 
+Coq < Require Import Classical.
+
 Coq < Section Exercise_7.
 
 Coq < Goal forall h c t f:Prop, ((h /\ (c /\ t)) /\ ~(~f /\ t)) -> f.
@@ -21,45 +23,37 @@ Unnamed_thm < intros.
   ============================
    f
 
-Unnamed_thm < elim H.
+Unnamed_thm < induction H.
 1 subgoal
 
   h : Prop
   c : Prop
   t : Prop
   f : Prop
-  H : (h /\ c /\ t) /\ ~ (~ f /\ t)
-  ============================
-   h /\ c /\ t -> ~ (~ f /\ t) -> f
-
-Unnamed_thm < intro.
-1 subgoal
-
-  h : Prop
-  c : Prop
-  t : Prop
-  f : Prop
-  H : (h /\ c /\ t) /\ ~ (~ f /\ t)
-  H0 : h /\ c /\ t
-  ============================
-   ~ (~ f /\ t) -> f
-
-Unnamed_thm < intro.
-1 subgoal
-
-  h : Prop
-  c : Prop
-  t : Prop
-  f : Prop
-  H : (h /\ c /\ t) /\ ~ (~ f /\ t)
-  H0 : h /\ c /\ t
-  H1 : ~ (~ f /\ t)
+  H : h /\ c /\ t
+  H0 : ~ (~ f /\ t)
   ============================
    f
 
+Unnamed_thm < tauto.
+No more subgoals.
+
+Unnamed_thm < Qed.
+intros.
+induction H.
+tauto.
+
+Unnamed_thm is defined
+
+Coq <
 
 
 *)
 
+Require Import Classical.
 Section Exercise_7.
 Goal forall h c t f:Prop, ((h /\ (c /\ t)) /\ ~(~f /\ t)) -> f.
+intros.
+induction H.
+tauto.
+Qed.
