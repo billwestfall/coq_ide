c8d6d8d6d269b0e4b0b227e2d7557b886d20158c
Bill Westfall <billwestfall@yahoo.com>
2016-08-29 15:50:54 -0700

    getting back to basics

---
+++ concise/misc/basic01.v
@@ -0,0 +1,35 @@
+Coq < Section BasicB.
+
+Coq < Goal B -> B.
+1 subgoal
+  
+  A : Prop
+  B : Prop
+  C : Prop
+  D : Prop
+  E : Prop
+  F : Prop
+  ============================
+   B -> B
+
+Unnamed_thm2 < intros.
+1 subgoal
+  
+  A : Prop
+  B : Prop
+  C : Prop
+  D : Prop
+  E : Prop
+  F : Prop
+  H : B
+  ============================
+   B
+
+Unnamed_thm2 < exact H.
+No more subgoals.
+
+Unnamed_thm2 < Qed.
+intros.
+exact H.
+
+Unnamed_thm2 is defined
