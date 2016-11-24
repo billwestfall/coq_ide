6d0461c7880ed077023d057ba3f654ae27f8d5e0
Bill Westfall <billwestfall@yahoo.com>
2016-11-24 13:50:09 -0800

    Create example04.v

---
+++ logic/chapt02/example04.v
@@ -0,0 +1,135 @@
+(precise)admin@localhost:~$ coqtop
+Welcome to Coq 8.3pl4 (April 2012)
+
+Coq < Section Examples.
+
+Coq < Require Import Classical.
+
+Coq < Variables P Q R S : Prop.
+P is assumed
+Q is assumed
+R is assumed
+S is assumed
+
+Coq < Goal ((P -> (Q /\ R)) /\ (R -> S)) -> (P -> S).
+1 subgoal
+  
+  P : Prop
+  Q : Prop
+  R : Prop
+  S : Prop
+  ============================
+   (P -> Q /\ R) /\ (R -> S) -> P -> S
+
+Unnamed_thm < intros.
+1 subgoal
+  
+  P : Prop
+  Q : Prop
+  R : Prop
+  S : Prop
+  H : (P -> Q /\ R) /\ (R -> S)
+  H0 : P
+  ============================
+   S
+
+Unnamed_thm < elim H.
+1 subgoal
+  
+  P : Prop
+  Q : Prop
+  R : Prop
+  S : Prop
+  H : (P -> Q /\ R) /\ (R -> S)
+  H0 : P
+  ============================
+   (P -> Q /\ R) -> (R -> S) -> S
+
+Unnamed_thm < intro.
+1 subgoal
+  
+  P : Prop
+  Q : Prop
+  R : Prop
+  S : Prop
+  H : (P -> Q /\ R) /\ (R -> S)
+  H0 : P
+  H1 : P -> Q /\ R
+  ============================
+   (R -> S) -> S
+
+Unnamed_thm < intro.
+1 subgoal
+  
+  P : Prop
+  Q : Prop
+  R : Prop
+  S : Prop
+  H : (P -> Q /\ R) /\ (R -> S)
+  H0 : P
+  H1 : P -> Q /\ R
+  H2 : R -> S
+  ============================
+   S
+
+Unnamed_thm < destruct H.
+1 subgoal
+  
+  P : Prop
+  Q : Prop
+  R : Prop
+  S : Prop
+  H : P -> Q /\ R
+  H3 : R -> S
+  H0 : P
+  H1 : P -> Q /\ R
+  H2 : R -> S
+  ============================
+   S
+
+Unnamed_thm < auto.
+1 subgoal
+  
+  P : Prop
+  Q : Prop
+  R : Prop
+  S : Prop
+  H : P -> Q /\ R
+  H3 : R -> S
+  H0 : P
+  H1 : P -> Q /\ R
+  H2 : R -> S
+  ============================
+   S
+
+Unnamed_thm < apply H2.
+1 subgoal
+  
+  P : Prop
+  Q : Prop
+  R : Prop
+  S : Prop
+  H : P -> Q /\ R
+  H3 : R -> S
+  H0 : P
+  H1 : P -> Q /\ R
+  H2 : R -> S
+  ============================
+   R
+
+Unnamed_thm < tauto.
+Proof completed.
+
+Unnamed_thm < Qed.
+intros.
+elim H.
+intro.
+intro.
+destruct H.
+auto.
+apply H2.
+tauto.
+
+Unnamed_thm is defined
+
+Coq < 
