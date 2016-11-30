fb2f4a04cde4e665c966a551aba9d0d2e85d7d16
Bill Westfall <billwestfall@yahoo.com>
2016-11-30 15:29:46 -0800

    Update basic10.v
    not eliminating the final OR solves the issue

--- concise/misc/basic10.v
+++ concise/misc/basic10.v
@@ -1,7 +1,7 @@
 MacBook-Air:~ billw$ /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/coqtop
 Welcome to Coq 8.4pl5 (October 2014)
 
-Coq < Section Examples.
+Coq < Section Esxamples.
 
 Coq < Require Import Classical.
 
@@ -68,135 +68,17 @@ Unnamed_thm < intro.
   ============================
    R \/ S
 
-Unnamed_thm < left.
-1 subgoal
-  
-  P : Prop
-  Q : Prop
-  R : Prop
-  S : Prop
-  H : (P \/ Q) /\ (P -> R) /\ (Q -> S)
-  H0 : P \/ Q
-  H1 : (P -> R) /\ (Q -> S)
-  ============================
-   R
-
-Unnamed_thm < destruct H1.
-1 subgoal
-  
-  P : Prop
-  Q : Prop
-  R : Prop
-  S : Prop
-  H : (P \/ Q) /\ (P -> R) /\ (Q -> S)
-  H0 : P \/ Q
-  H1 : P -> R
-  H2 : Q -> S
-  ============================
-   R
-
-Unnamed_thm < firstorder.
-1 subgoal
-  
-  P : Prop
-  Q : Prop
-  R : Prop
-  S : Prop
-  H1 : P -> R
-  H3 : P -> R
-  H : Q
-  H0 : Q
-  H4 : S
-  H2 : S
-  ============================
-   R
-
 Unnamed_thm < tauto.
-Toplevel input, characters 0-5:
-> tauto.
-> ^^^^^
-Error: Classical tauto failed.
+No more subgoals.
 
-Unnamed_thm < auto.
-1 subgoal
-  
-  P : Prop
-  Q : Prop
-  R : Prop
-  S : Prop
-  H1 : P -> R
-  H3 : P -> R
-  H : Q
-  H0 : Q
-  H4 : S
-  H2 : S
-  ============================
-   R
+Unnamed_thm < Qed.
+intros.
+elim H.
+intro.
+intro.
+tauto.
 
-Unnamed_thm < apply H1.
-1 subgoal
-  
-  P : Prop
-  Q : Prop
-  R : Prop
-  S : Prop
-  H1 : P -> R
-  H3 : P -> R
-  H : Q
-  H0 : Q
-  H4 : S
-  H2 : S
-  ============================
-   P
+Unnamed_thm is defined
 
-Unnamed_thm < firstorder.
-1 subgoal
-  
-  P : Prop
-  Q : Prop
-  R : Prop
-  S : Prop
-  H1 : P -> R
-  H3 : P -> R
-  H : Q
-  H0 : Q
-  H4 : S
-  H2 : S
-  ============================
-   P
+Coq < 
 
-Unnamed_thm < tauto.
-Toplevel input, characters 0-5:
-> tauto.
-> ^^^^^
-Error: Classical tauto failed.
-
-Unnamed_thm < auto.
-1 subgoal
-  
-  P : Prop
-  Q : Prop
-  R : Prop
-  S : Prop
-  H1 : P -> R
-  H3 : P -> R
-  H : Q
-  H0 : Q
-  H4 : S
-  H2 : S
-  ============================
-   P
-
-Unnamed_thm < destruct H3. 
-Toplevel input, characters 0-11:
-> destruct H3.
-> ^^^^^^^^^^^
-Error: Not an inductive product.
-
-Unnamed_thm < elim H3.
-Toplevel input, characters 0-7:
-> elim H3.
-> ^^^^^^^
-Error: Not an inductive product.
-
-Unnamed_thm < 
