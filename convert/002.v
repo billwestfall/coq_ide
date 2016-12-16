15357f8bbad7275dea081c774675249088040b8a
Bill Westfall <billwestfall@yahoo.com>
2016-12-16 15:27:17 -0800

    Create 002.v
    some practice before attempting any conversions

---
+++ convert/002.v
@@ -0,0 +1,117 @@
+MacBook-Air:~ billw$ /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/coqtop
+Welcome to Coq 8.4pl5 (October 2014)
+
+Coq < Section predicate_calculus.
+
+Coq < Variable D : Set.
+D is assumed
+
+Coq < Variable R : D -> D -> Prop.
+R is assumed
+
+Coq < Section R_sym_trans.
+
+Coq < Hypothesis R_symmetric : forall x y:D, R x y -> R y x.
+R_symmetric is assumed
+
+Coq < Hypothesis R_transitive : forall x y z:D, R x y -> R y z -> R x z.
+R_transitive is assumed
+
+Coq < Lemma refl_if : forall x:D, (exists y, R x y) -> R x x.
+1 subgoal
+  
+  D : Set
+  R : D -> D -> Prop
+  R_symmetric : forall x y : D, R x y -> R y x
+  R_transitive : forall x y z : D, R x y -> R y z -> R x z
+  ============================
+   forall x : D, (exists y : D, R x y) -> R x x
+
+refl_if < Check ex.
+ex
+     : forall A : Type, (A -> Prop) -> Prop
+
+refl_if < intros x x_Rlinked.
+1 subgoal
+  
+  D : Set
+  R : D -> D -> Prop
+  R_symmetric : forall x y : D, R x y -> R y x
+  R_transitive : forall x y z : D, R x y -> R y z -> R x z
+  x : D
+  x_Rlinked : exists y : D, R x y
+  ============================
+   R x x
+
+refl_if < elim x_Rlinked.
+1 subgoal
+  
+  D : Set
+  R : D -> D -> Prop
+  R_symmetric : forall x y : D, R x y -> R y x
+  R_transitive : forall x y z : D, R x y -> R y z -> R x z
+  x : D
+  x_Rlinked : exists y : D, R x y
+  ============================
+   forall x0 : D, R x x0 -> R x x
+
+refl_if < intros y Rxy.
+1 subgoal
+  
+  D : Set
+  R : D -> D -> Prop
+  R_symmetric : forall x y : D, R x y -> R y x
+  R_transitive : forall x y z : D, R x y -> R y z -> R x z
+  x : D
+  x_Rlinked : exists y : D, R x y
+  y : D
+  Rxy : R x y
+  ============================
+   R x x
+
+refl_if < apply R_transitive with y.
+2 subgoals
+  
+  D : Set
+  R : D -> D -> Prop
+  R_symmetric : forall x y : D, R x y -> R y x
+  R_transitive : forall x y z : D, R x y -> R y z -> R x z
+  x : D
+  x_Rlinked : exists y : D, R x y
+  y : D
+  Rxy : R x y
+  ============================
+   R x y
+
+subgoal 2 is:
+ R y x
+
+refl_if < assumption.
+1 subgoal
+  
+  D : Set
+  R : D -> D -> Prop
+  R_symmetric : forall x y : D, R x y -> R y x
+  R_transitive : forall x y z : D, R x y -> R y z -> R x z
+  x : D
+  x_Rlinked : exists y : D, R x y
+  y : D
+  Rxy : R x y
+  ============================
+   R y x
+
+refl_if < apply R_symmetric; assumption.
+No more subgoals.
+
+refl_if < Qed.
+intros x x_Rlinked.
+elim x_Rlinked.
+intros y Rxy.
+apply R_transitive with y.
+ assumption.
+
+ apply R_symmetric; assumption.
+
+refl_if is defined
+
+Coq < 
