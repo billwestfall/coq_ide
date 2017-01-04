545298b7d32524bd744495a84503b8912db03d03
Bill Westfall <billwestfall@yahoo.com>
2017-01-03 18:35:53 -0800

    Create 001.v
    testing out the crush tactic

---
+++ cptd/chapt01/001.v
@@ -0,0 +1,22 @@
+Coq < Section Crush_test.
+
+Coq < Load CpdtTactics.
+
+Coq < Variables B: Prop.
+Warning: Keywords Variables/Hypotheses/Parameters expect more than one assumption
+B is assumed
+
+Coq < Goal B -> B.
+1 subgoal
+  
+  B : Prop
+  ============================
+   B -> B
+
+Unnamed_thm < crush.
+No more subgoals.
+
+Unnamed_thm < Qed.
+crush.
+
+Unnamed_thm is defined
