f276b17f30dbaf379614d51eaff3ea445ad95f2c
Bill Westfall <billwestfall@yahoo.com>
2017-01-08 21:24:22 -0800

    Create pred021.v
    similar to previous but with corrected expectation

---
+++ cptd/chapt01/pred021.v
@@ -0,0 +1,28 @@
+Welcome to Coq 8.4pl4 (August 2014)
+
+Coq < Section Predicate021.
+
+Coq < Require Import Bool Arith List.
+[Loading ML file z_syntax_plugin.cmxs ... done]
+[Loading ML file quote_plugin.cmxs ... done]
+[Loading ML file newring_plugin.cmxs ... done]
+
+Coq < Load CpdtTactics.
+
+Coq < Set Implicit Arguments.
+
+Coq < Set Asymmetric Patterns.
+
+Coq < Goal forall (W : Type) (B : W->Prop), (exists w : W, B w <-> ~B w).
+1 subgoal
+  
+  ============================
+   forall (W : Type) (B : W -> Prop), exists w : W, B w <-> ~ B w
+
+Unnamed_thm < crush.
+1 subgoal
+  
+  W : Type
+  B : W -> Prop
+  ============================
+   exists w : W, B w <-> B w -> False
