e4f5ec0aa51d8fc72fa817479442356bfea4b870
Bill Westfall <billwestfall@yahoo.com>
2017-01-09 20:20:58 -0800

    Create pred029.v

---
+++ cptd/chapt01/pred029.v
@@ -0,0 +1,33 @@
+Welcome to Coq 8.4pl4 (August 2014)
+
+Coq < Section Predicate029.
+
+Coq < Require Import Bool Arith List.
+[Loading ML file z_syntax_plugin.cmxs ... done]
+[Loading ML file quote_plugin.cmxs ... done]
+[Loading ML file newring_plugin.cmxs ... done]
+
+Coq < Load CpdtTactics.
+
+Coq < Set Implicit Arguments.
+
+Coq < Set Asymmetric Arguments.
+
+Coq < Goal forall (X : Type) (F G : X->Prop), (forall x : X, F x) -> (exists! x : X, ~(F x \/ G x)).
+1 subgoal
+  
+  ============================
+   forall (X : Type) (F G : X -> Prop),
+   (forall x : X, F x) -> exists ! x : X, ~ (F x \/ G x)
+
+Unnamed_thm < crush.
+1 subgoal
+  
+  X : Type
+  F : X -> Prop
+  G : X -> Prop
+  H : forall x : X, F x
+  ============================
+   exists ! x : X, F x \/ G x -> False
+
+Unnamed_thm < 
