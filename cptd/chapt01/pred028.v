d159f8b0e3834a01aca1d1de9fa57636cf76ddc4
Bill Westfall <billwestfall@yahoo.com>
2017-01-09 20:11:31 -0800

    Create pred028.v

---
+++ cptd/chapt01/pred028.v
@@ -0,0 +1,29 @@
+Welcome to Coq 8.4pl4 (August 2014)
+
+Coq < Section Predicate028.
+
+Coq < Require Import Bool Arith List.
+[Loading ML file z_syntax_plugin.cmxs ... done]
+[Loading ML file quote_plugin.cmxs ... done]
+[Loading ML file newring_plugin.cmxs ... done]
+
+Coq < Load CpdtTactics.
+
+Coq < Set Implicit Arguments.
+
+Coq < Set Asymmetric Arguments.
+
+Coq < Goal forall (X : Type) (F G : X->Prop), (forall x : X, F x -> G x) -> (forall x : X, (~G x -> ~F x)).
+1 subgoal
+  
+  ============================
+   forall (X : Type) (F G : X -> Prop),
+   (forall x : X, F x -> G x) -> forall x : X, ~ G x -> ~ F x
+
+Unnamed_thm < crush.
+No more subgoals.
+
+Unnamed_thm < Qed.
+crush.
+
+Unnamed_thm is defined
