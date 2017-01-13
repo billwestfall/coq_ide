03ecd030f43a0c3ec0396d8b90c2357c278c1450
Bill Westfall <billwestfall@yahoo.com>
2017-01-12 19:47:07 -0800

    Create pred041.v

---
+++ cptd/chapt01/pred041.v
@@ -0,0 +1,37 @@
+Welcome to Coq 8.4pl4 (August 2014)
+
+Coq < Section Predicate041.
+
+Coq < Require Import Bool Arith List.
+[Loading ML file z_syntax_plugin.cmxs ... done]
+[Loading ML file quote_plugin.cmxs ... done]
+[Loading ML file newring_plugin.cmxs ... done]
+
+Coq < Load CpdtTactics.
+
+Coq < Set Implicit Arguments.
+
+Coq < Set Asymmetric Patterns.
+
+Coq < Goal forall (X : Type) (P Q: X->Prop), ( forall x : X, Q x \/ P x ) -> ( exists x : X, Px ).
+Toplevel input, characters 87-89:
+> Goal forall (X : Type) (P Q: X->Prop), ( forall x : X, Q x \/ P x ) -> ( exists x : X, Px ).
+>                                                                                        ^^
+Error: The reference Px was not found in the current environment.
+
+Coq < Goal forall (X : Type) (P Q: X->Prop), ( forall x : X, Q x \/ P x ) -> ( exists x : X, P x ).
+1 subgoal
+  
+  ============================
+   forall (X : Type) (P Q : X -> Prop),
+   (forall x : X, Q x \/ P x) -> exists x : X, P x
+
+Unnamed_thm < crush.
+1 subgoal
+  
+  X : Type
+  P : X -> Prop
+  Q : X -> Prop
+  H : forall x : X, Q x \/ P x
+  ============================
+   exists x : X, P x
