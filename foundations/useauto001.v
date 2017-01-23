fb6a9ad3081c7a80e6aa16bb3bcd69d16d488b82
Bill Westfall <billwestfall@yahoo.com>
2017-01-23 12:52:16 -0800

    Create useauto001.v

---
+++ foundations/useauto001.v
@@ -0,0 +1,28 @@
+Welcome to Coq ciosx:/builds/workspace/coq-8.5pl3-macos,(detached from 2290dbb) (2290dbb9c95b63e693ced647731623e64297f5c8)
+
+Coq < Section UseAuto001.
+
+Coq < Require Import Arith Classical.
+[Loading ML file z_syntax_plugin.cmxs ... done]
+[Loading ML file quote_plugin.cmxs ... done]
+[Loading ML file newring_plugin.cmxs ... done]
+<W> Grammar extension: in [tactic:simple_tactic], some rule has been masked
+<W> Grammar extension: in [tactic:simple_tactic], some rule has been masked
+<W> Grammar extension: in [tactic:simple_tactic], some rule has been masked
+<W> Grammar extension: in [tactic:simple_tactic], some rule has been masked
+<W> Grammar extension: in [tactic:simple_tactic], some rule has been masked
+
+Coq < Goal 2 + 3 = 5.
+1 subgoal
+  
+  ============================
+  2 + 3 = 5
+
+Unnamed_thm < auto.
+No more subgoals.
+
+Unnamed_thm < Qed.
+auto.
+
+Qed.
+Unnamed_thm is defined
