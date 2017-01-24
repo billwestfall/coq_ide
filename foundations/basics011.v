c54cde72a3bdd0de12acd41215b5cc669f876996
Bill Westfall <billwestfall@yahoo.com>
2017-01-24 15:35:38 -0800

    Create basics011.v

---
+++ foundations/basics011.v
@@ -0,0 +1,61 @@
+Welcome to Coq ciosx:/builds/workspace/coq-8.5pl3-macos,(detached from 2290dbb) (2290dbb9c95b63e693ced647731623e64297f5c8)
+
+Coq < Theorem plus_1_1 : forall n : nat, 1 + n = S n.
+1 subgoal
+  
+  ============================
+  forall n : nat, 1 + n = S n
+
+plus_1_1 < Proof.
+1 subgoal
+  
+  ============================
+  forall n : nat, 1 + n = S n
+
+plus_1_1 < intros n.
+1 subgoal
+  
+  n : nat
+  ============================
+  1 + n = S n
+
+plus_1_1 < reflexivity.
+No more subgoals.
+
+plus_1_1 < Qed.
+Proof.
+intros n.
+reflexivity.
+
+Qed.
+plus_1_1 is defined
+
+Coq < Theorem mult_0_1 : forall n : nat , 0 * n = 0.
+1 subgoal
+  
+  ============================
+  forall n : nat, 0 * n = 0
+
+mult_0_1 < Proof.
+1 subgoal
+  
+  ============================
+  forall n : nat, 0 * n = 0
+
+mult_0_1 < intros n.
+1 subgoal
+  
+  n : nat
+  ============================
+  0 * n = 0
+
+mult_0_1 < reflexivity.
+No more subgoals.
+
+mult_0_1 < Qed.
+Proof.
+intros n.
+reflexivity.
+
+Qed.
+mult_0_1 is defined
