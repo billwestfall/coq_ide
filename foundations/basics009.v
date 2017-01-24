cfda8e702ff9bb5766ce89641d3497e34259ba1e
Bill Westfall <billwestfall@yahoo.com>
2017-01-24 15:29:10 -0800

    Create basics009.v

---
+++ foundations/basics009.v
@@ -0,0 +1,39 @@
+Welcome to Coq ciosx:/builds/workspace/coq-8.5pl3-macos,(detached from 2290dbb) (2290dbb9c95b63e693ced647731623e64297f5c8)
+
+Coq < Theorem plus_O_n : forall n : nat, 0 + n = n.
+1 subgoal
+  
+  ============================
+  forall n : nat, 0 + n = n
+
+plus_O_n < Proof.
+1 subgoal
+  
+  ============================
+  forall n : nat, 0 + n = n
+
+plus_O_n < intros n.
+1 subgoal
+  
+  n : nat
+  ============================
+  0 + n = n
+
+plus_O_n < simpl.
+1 subgoal
+  
+  n : nat
+  ============================
+  n = n
+
+plus_O_n < reflexivity.
+No more subgoals.
+
+plus_O_n < Qed.
+Proof.
+intros n.
+simpl.
+reflexivity.
+
+Qed.
+plus_O_n is defined
