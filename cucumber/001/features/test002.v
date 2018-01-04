4149de4cfae4e71cac72e538dce965087a30a517
Bill Westfall <billw@concur.com>
2018-01-04 14:29:12 -0800

    Add working file and one deliberate fail for test

--- cucumber/001/features/test002.v
+++ cucumber/001/features/test002.v
@@ -1,4 +1,4 @@
-Theorem Restart_03 : forall a b c:Prop, ((a \/ b) /\ (b \/ c)) -> (a \/ b \/ c).
+Theorem Restart_03 : forall a b c:Prop, ((a \/ b) /\ (b \/ c)) -> (a \/ b \/ d).
 Proof.
 Add LoadPath "/Users/Shared/Files/cptd/src/" as CpdtTactics.
 Require Import Classical.
